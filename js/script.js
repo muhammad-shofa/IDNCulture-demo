7fdcfd1585e4e371580d03905cf4ace889069b60
Muhammad Shofa <95084317+muhammad-shofa@users.noreply.github.com>
2023-11-11 22:56:59 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,4 +7,16 @@ window.addEventListener("scroll", () => {
   }
 });
 
-
+// icon navbar
+let menuIcon = document.getElementById("menu-icon");
+menuIcon.addEventListener("click", function () {
+  this.classList.toggle("close-icon");
+  const navUl = document.getElementById("nav-ul");
+  if (navUl.classList.contains("hidden-ul")) {
+    navUl.classList.remove("hidden-ul");
+    navUl.classList.add("show-ul");
+  } else {
+    navUl.classList.remove("show-ul");
+    navUl.classList.add("hidden-ul");
+  }
+});

e4a397c060c7f4a94b36e3b933ce692dab852634
Muhammad Shofa <95084317+muhammad-shofa@users.noreply.github.com>
2023-11-13 18:15:21 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,9 +22,27 @@ menuIcon.addEventListener("click", function () {
 });
 
 // fav destination
+// 1
 let favDestination = document.getElementById("favorite-1");
 let emptyHeart = document.getElementById("empty-heart");
 let fullHeart = document.getElementById("full-heart");
+// 2
+let favDestination2 = document.getElementById("favorite-2");
+let emptyHeart2 = document.getElementById("empty-heart-2");
+let fullHeart2 = document.getElementById("full-heart-2");
+// 3
+let favDestination3 = document.getElementById("favorite-3");
+let emptyHeart3 = document.getElementById("empty-heart-3");
+let fullHeart3 = document.getElementById("full-heart-3");
+// 4
+let favDestination4 = document.getElementById("favorite-4");
+let emptyHeart4 = document.getElementById("empty-heart-4");
+let fullHeart4 = document.getElementById("full-heart-4");
+// 5
+let favDestination5 = document.getElementById("favorite-5");
+let emptyHeart5 = document.getElementById("empty-heart-5");
+let fullHeart5 = document.getElementById("full-heart-5");
+
 favDestination.addEventListener("click", () => {
   if (emptyHeart.style.display == "block") {
     fullHeart.style.display = "block";
@@ -35,9 +53,6 @@ favDestination.addEventListener("click", () => {
   }
 });
 
-let favDestination2 = document.getElementById("favorite-2");
-let emptyHeart2 = document.getElementById("empty-heart-2");
-let fullHeart2 = document.getElementById("full-heart-2");
 favDestination2.addEventListener("click", () => {
   if (emptyHeart2.style.display == "block") {
     fullHeart2.style.display = "block";
@@ -48,9 +63,6 @@ favDestination2.addEventListener("click", () => {
   }
 });
 
-let favDestination3 = document.getElementById("favorite-3");
-let emptyHeart3 = document.getElementById("empty-heart-3");
-let fullHeart3 = document.getElementById("full-heart-3");
 favDestination3.addEventListener("click", () => {
   if (emptyHeart3.style.display == "block") {
     fullHeart3.style.display = "block";
@@ -61,9 +73,6 @@ favDestination3.addEventListener("click", () => {
   }
 });
 
-let favDestination4 = document.getElementById("favorite-4");
-let emptyHeart4 = document.getElementById("empty-heart-4");
-let fullHeart4 = document.getElementById("full-heart-4");
 favDestination4.addEventListener("click", () => {
   if (emptyHeart4.style.display == "block") {
     fullHeart4.style.display = "block";
@@ -74,9 +83,6 @@ favDestination4.addEventListener("click", () => {
   }
 });
 
-let favDestination5 = document.getElementById("favorite-5");
-let emptyHeart5 = document.getElementById("empty-heart-5");
-let fullHeart5 = document.getElementById("full-heart-5");
 favDestination5.addEventListener("click", () => {
   if (emptyHeart5.style.display == "block") {
     fullHeart5.style.display = "block";

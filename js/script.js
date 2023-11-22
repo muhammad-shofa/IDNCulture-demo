2e2810ec3845ced3d8685ca8ae791b13c7f994ca
Muhammad Shofa <95084317+muhammad-shofa@users.noreply.github.com>
2023-11-22 20:09:18 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,51 @@
 // loading
-let loading = document.getElementById("loading");
-let allContent = document.getElementById("all-content");
+const loading = document.getElementById("loading");
+const allContent = document.getElementById("all-content");
+// sticky nav
+let nav = document.querySelector("nav");
+let containerNav = document.getElementById("container-nav");
+// back to top
+let backToTop = document.getElementById("back-to-top");
+// icon navbar
+let menuIcon = document.getElementById("menu-icon");
+// dropdown
+let triggerDropdown = document.getElementById("dropdown-explore");
+let textCulture = document.getElementById("text-culture");
+let containerDropdown = document.getElementById("container-dropdown");
+// variable fav
+// 1
+let favDestination = document.getElementById("favorite-1");
+let emptyHeart = document.getElementById("empty-heart");
+let fullHeart = document.getElementById("full-heart");
+// 2
+let favDestination2 = document.getElementById("favorite-2");
+let emptyHeart2 = document.getElementById("empty-heart-2");
+let fullHeart2 = document.getElementById("full-heart-2");
+// 3
+let favDestination3 = document.getElementById("favorite-3");
+let emptyHeart3 = document.getElementById("empty-heart-3");
+let fullHeart3 = document.getElementById("full-heart-3");
+// 4
+let favDestination4 = document.getElementById("favorite-4");
+let emptyHeart4 = document.getElementById("empty-heart-4");
+let fullHeart4 = document.getElementById("full-heart-4");
+// 5
+let favDestination5 = document.getElementById("favorite-5");
+let emptyHeart5 = document.getElementById("empty-heart-5");
+let fullHeart5 = document.getElementById("full-heart-5");
+// gallery animation
+let copy1 = document.querySelector(".slide-container-1").cloneNode(true);
+let copy2 = document.querySelector(".slide-container-2").cloneNode(true);
+let copy3 = document.querySelector(".slide-container-3").cloneNode(true);
+// popup gallery
+let imgPopupGallery = document.getElementById("img-popup-gallery");
+// update
+let update = document.getElementById("update");
+let closeUpdate = document.getElementById("close-update");
+
+closeUpdate.addEventListener("click", () => {
+  update.style.display = "none";
+});
 
 document.addEventListener("DOMContentLoaded", loadingTime);
 function loadingTime() {
@@ -13,37 +58,47 @@ function loadingTime() {
   }
 }
 
-// sticky nav
-let nav = document.querySelector("nav");
-let containerNav = document.getElementById("container-nav");
 window.addEventListener("scroll", () => {
   if (document.documentElement.scrollTop > 20) {
     containerNav.classList.add("sticky");
+    containerDropdown.style.backgroundColor = "var(--white-accent)";
   } else {
     containerNav.classList.remove("sticky");
+    containerDropdown.style.backgroundColor = "rgba(255, 255, 255, 0.7)";
   }
 });
 
-// icon navbar
-let menuIcon = document.getElementById("menu-icon");
-// let lineMenuIcon1 = document.getElementById("line-1");
-// let lineMenuIcon2 = document.getElementById("line-2");
-// let lineMenuIcon3 = document.getElementById("line-3");
+triggerDropdown.addEventListener("mouseover", showDropdown);
+containerDropdown.addEventListener("mouseover", showDropdown);
+triggerDropdown.addEventListener("mouseout", hiddenDropdown);
+containerDropdown.addEventListener("mouseout", hiddenDropdown);
+function showDropdown() {
+  containerDropdown.style.marginTop = "0px";
+  textCulture.style.color = "var(--secondary)";
+}
+
+function hiddenDropdown() {
+  containerDropdown.style.marginTop = "-1000px";
+  textCulture.style.color = "";
+}
+
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 700) {
+    backToTop.style.opacity = 0.8;
+  } else {
+    backToTop.style.opacity = 0;
+  }
+});
+
 menuIcon.addEventListener("click", function () {
   this.classList.toggle("close-icon");
   const navUl = document.getElementById("nav-ul");
   if (navUl.classList.contains("hidden-ul")) {
     navUl.classList.remove("hidden-ul");
     navUl.classList.add("show-ul");
-    // lineMenuIcon1.style.backgroundColor = "var(--black-accent)";
-    // lineMenuIcon2.style.backgroundColor = "var(--black-accent)";
-    // lineMenuIcon3.style.backgroundColor = "var(--black-accent)";
   } else {
     navUl.classList.remove("show-ul");
     navUl.classList.add("hidden-ul");
-    // lineMenuIcon1.style.backgroundColor = "var(--white-accent)";
-    // lineMenuIcon2.style.backgroundColor = "var(--white-accent)";
-    // lineMenuIcon3.style.backgroundColor = "var(--white-accent)";
   }
 });
 
@@ -74,33 +129,16 @@ menuIcon.addEventListener("click", function () {
 //   }
 // });
 
-// variable popup and fav
-// 1
-let favDestination = document.getElementById("favorite-1");
-let emptyHeart = document.getElementById("empty-heart");
-let fullHeart = document.getElementById("full-heart");
-// 2
-let favDestination2 = document.getElementById("favorite-2");
-let emptyHeart2 = document.getElementById("empty-heart-2");
-let fullHeart2 = document.getElementById("full-heart-2");
-// 3
-let favDestination3 = document.getElementById("favorite-3");
-let emptyHeart3 = document.getElementById("empty-heart-3");
-let fullHeart3 = document.getElementById("full-heart-3");
-// 4
-let favDestination4 = document.getElementById("favorite-4");
-let emptyHeart4 = document.getElementById("empty-heart-4");
-let fullHeart4 = document.getElementById("full-heart-4");
-// 5
-let favDestination5 = document.getElementById("favorite-5");
-let emptyHeart5 = document.getElementById("empty-heart-5");
-let fullHeart5 = document.getElementById("full-heart-5");
-
 // function popup explore
 function showPopup(popupId) {
   let containerPopup = document.getElementById("container-popup-" + popupId);
   if (containerPopup.style.display == "none") {
     containerPopup.style.display = "block";
+    containerPopup.addEventListener("click", (event) => {
+      if (event.target == containerPopup) {
+        containerPopup.style.display = "none";
+      }
+    });
   } else {
     containerPopup.style.display = "none";
   }
@@ -167,11 +205,29 @@ favDestination5.addEventListener("click", () => {
 });
 
 // gallery animation
-let copy1 = document.querySelector(".slide-container-1").cloneNode(true);
 document.querySelector(".img-gallery-1").appendChild(copy1);
-// gallery animation 2
-let copy2 = document.querySelector(".slide-container-2").cloneNode(true);
 document.querySelector(".img-gallery-2").appendChild(copy2);
-// gallery animation 3
-let copy3 = document.querySelector(".slide-container-3").cloneNode(true);
 document.querySelector(".img-gallery-3").appendChild(copy3);
+
+// popop gallery
+
+function showPopupGallery(idPopupGalleryName) {
+  let popupGallery = document.getElementById(
+    "popup-gallery-" + idPopupGalleryName
+  );
+  if (popupGallery.style.display == "none") {
+    popupGallery.style.display = "block";
+    popupGallery.addEventListener("click", closePopupGallery);
+    function closePopupGallery(event) {
+      if (event.target == popupGallery) {
+        popupGallery.style.display = "none";
+      }
+    }
+    // document.addEventListener("click", closePopup);
+  } else {
+    popupGallery.style.display = "none";
+  }
+}
+
+// popup gallery
+// popupGallery.addEventListener("click", closePopupGallery);
